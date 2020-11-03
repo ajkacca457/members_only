4f01b4ba4fdbaeb778e2bbb372dc2793be68d488
Avijit <ajkacca@gmail.com>
2020-11-03 20:59:39 +0200

    user authentication add

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true,
+                    uniqueness: { case_sensitive: false }
+
+  validates :username, presence: true,
+             uniqueness: { case_sensitive: false }
+
+
+  validates :email, presence:true
+
+  has_many :posts
 end
